cc8d82c464ef358c51d31f95c029c925f99c98d3
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2021-09-13 13:33:38 -0300

    Full Commit

---
+++ src/componentes/Primeiro.jsx
@@ -0,0 +1,4 @@
+// Como no outro exemplo lá atrás, não se mostrou mais necessário
+import React from 'react';
+
+export default () => <h1>Primeiro</h1>;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
+// Não é mais necessário
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import Primeiro from './componentes/Primeiro';
+// import BomDia from './componentes/BomDia';
+// import { BoaTarde, BoaNoite } from './componentes/Multiplos';
+// import Multi from './componentes/Multilpos';
+// import Saudacao from './componentes/Saudacao';
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
+// <BomDia></BomDia> ou <BomDia />
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <div>
+    <Pai nome="Paulo" sobrenome="Silva">
+      <Filho nome="Pedro" />
+      <Filho nome="Paulo" />
+      <Filho nome="Carla" />
+    </Pai>
+  </div>
+, document.getElementById('root'));
